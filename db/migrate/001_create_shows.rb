d3bd05be0a3f9f348c1ca62e0343002a7bdd12b6
Thomas Pope <tpope0928@gmail.com>
2019-01-14 14:40:20 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,10 +1,10 @@
 class CreateShows < ActiveRecord::Migration
   def change
     create_table :shows do |t|
-      t.string :name,
-      t.string :network,
-      t.string :day,
-      t.integer :integer
+      t.string :name
+      t.string :day
+      t.string :network
+      t.integer :rating
     end
   end
 end
